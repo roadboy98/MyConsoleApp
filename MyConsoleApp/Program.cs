79c0ac7ba9d757529fb8634e999bd3bf90f6042c
reece <reece@DESKTOP-0E2MFA9>
2022-04-16 19:01:42 +0100

    Add project files.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿//Declaring 'ants' variable and setting it to 0
+int ants = 0;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("There are " + ants + " ants in your back garden.");
+Console.WriteLine("Type the number of ants you see and press enter");
+
+//Gets the user input and writes to console
+ants = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("There is now " + ants + " ants in the garden!");
